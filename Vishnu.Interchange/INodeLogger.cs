e822a22143905d5c081e77c0c4b42b1895971744
Erik Nagel <erik@reallyhuman.net>
2021-07-04 23:25:45 +0200

    Plugins aktualisiert.

--- Vishnu.Interchange/INodeLogger.cs
+++ Vishnu.Interchange/INodeLogger.cs
@@ -17,7 +17,7 @@
         /// </summary>
         /// <param name="loggerParameters">Spezifische Aufrufparameter oder null.</param>
         /// <param name="treeParameters">Für den gesamten Tree gültige Parameter oder null.</param>
-        /// <param name="treeEvent">Klasse mit Informationen über das Ereignis.</param>
+        /// <param name="treeEvent">Objekt mit Informationen über das Ereignis.</param>
         /// <param name="additionalEventArgs">Enthält z.B. beim Event 'Exception' die zugehörige Exception.</param>
         void Log(object loggerParameters, TreeParameters treeParameters, TreeEvent treeEvent, object additionalEventArgs);
     }
